8d6fcdbc6b506bedde0bc4d98680e334d5f1eb23
57KayBel0 <makgaekabelomotshabi.kg@gmail.com>
2023-02-18 07:05:29 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,18 +5,10 @@
   */
 int main(void)
 {
-	/*int integerType;
-	float floatType;
-	long int longintType;
-	long long int longlongintType;
-	char charType;*/
-
-    /* Sizeof operator is used to evaluate the size of a variable*/
-    printf("Size of char: %ld byte(s)\n", sizeof(char));
-    printf("Size of int: %ld bytes(s)\n", sizeof(int));
-    printf("Size of long int: %ld bytes(s)\n", sizeof(long int));
-    printf("Size of long long int: %ld bytes(s)\n", sizeof(long long int));
-    printf("Size of float: %ld bytes(s)\n", sizeof(float));
-
+    printf("Size of char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+    printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+    printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+    printf("Size of a float: %lu bytes(s)", sizeof(float));
     return (0);
 }
